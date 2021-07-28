3e179b4133f8016ce2fd30fc08da77d4c1c8012b
mahyaam <7mahya.amiri@gmail.com>
2021-07-28 15:03:58 +0430

    Array

---
+++ Chapter04/Chapter04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = { 10, 20, 30, 40, 50 };
+
+            int intA = 10;
+            int intB = 20;
+            int intC = 30;
+
+            Console.ReadLine();
+        }
+    }
+}
