b1873a2e214295e9b639947c8e705161c6f1366d
mahyaam <7mahya.amiri@gmail.com>
2021-07-28 16:13:01 +0430

    Private Field

---
+++ Chapter09/Chapter09/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Chapter09
+{
+    public class Person
+    {
+        public int Age;
+        public string FullName;
+
+        private int _somePrivateField1;
+        int _somePrivateField2; 
+
+        public Person(string fullName, int age)
+        {
+            Age = age;
+            FullName = fullName;
+        }
+
+
+        public void ShowInfo()
+        {
+            _somePrivateField1 = 20;
+            System.Console.WriteLine("Full Name: {0}, Age: {1}", FullName, Age);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Wrong
+            // Person P = new Person();
+            // P.Age = 20;
+            // P.FullName = "Ali Reza Alavi";
+            // P.ShowInfo();
+
+            Person P = new Person("Sara Ahmadi", 30);
+            P.ShowInfo();
+
+            System.Console.ReadLine();
+        }
+    }
+}
