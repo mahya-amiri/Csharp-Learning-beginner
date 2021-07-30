71165dbca8dbb32e497c1a6c119016f27ffb6eed
mahyaam <7mahya.amiri@gmail.com>
2021-07-30 18:22:39 +0430

    "static class"

---
+++ Chapter14/Chapter14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter14
+{
+    public class SomeClass
+    {
+        public int x;
+        private int _y;
+
+        public static int m;
+        private static int _n;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass.m = 20;
+            SomeClass obj = new SomeClass();
+            obj.x = 10;
+            Console.ReadLine();
+        }
+    }
+}
