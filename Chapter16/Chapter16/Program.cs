afd04d22599fc117526262802d3982a80d709f95
mahyaam <7mahya.amiri@gmail.com>
2021-07-30 19:41:46 +0430

    const, readonly

---
+++ Chapter16/Chapter16/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Chapter16
+{
+    public class Utility
+    {
+        public const int a = 10;
+        public int b;
+        public readonly int c;
+        public readonly int d = 2;
+        public readonly int e = 7;
+
+        public Utility()
+        {
+            c = 1;
+        }
+        public Utility(int constant1, int constant2)
+        {
+            c = constant1;
+            e = constant2;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int intNumber;
+            Utility SomeObject = new Utility();
+
+            // Wrong Usage!
+            // Utilities.a = 2;     CONST
+            intNumber = Utility.a;
+            // Wrong Usage!
+            // SomeObject.a = 2;
+            // int intNumber = SomeObject.a;
+
+            SomeObject.b = 1;
+            intNumber = SomeObject.b;
+
+            // Wrong Usage!
+            // SomeObject.c = 2; // ReadOnly
+            intNumber = SomeObject.c;
+
+            System.Console.ReadLine();
+    }
+}
